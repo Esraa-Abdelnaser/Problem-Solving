d55a1aa2c69f99c606d69b0b712e29c488d45961
Esraa Abdelnaser <esraaabdalnaser@gmail.com>
2022-12-02 19:08:35 +0200

    Task 13

---
+++ 013-Delete duplicate-value nodes from a sorted linked list _ HackerRank.cpp
@@ -0,0 +1,13 @@
+SinglyLinkedListNode* removeDuplicates(SinglyLinkedListNode* llist) {
+    
+    SinglyLinkedListNode* temp=llist;
+    while(temp->next !=NULL){
+        if (temp->data==temp->next->data){
+            temp->next=temp->next->next;
+        }
+        else{
+            temp=temp->next;
+        }
+    }
+    return llist;
+}
